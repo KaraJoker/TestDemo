ba7f31cbc1ebc7837467277f705f721e4db7e669
KaraJoker <1205789180@qq.com>
2019-10-11 18:12:59 +0800

    快照文件生成控制类

---
+++ test_gemo/src/main/java/com/cn/company/controller/SnapshotController.java
@@ -0,0 +1,28 @@
+package com.cn.company.controller;
+
+import com.cn.company.cqrs.c.service.disruptor.CoreSnapshotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/snapshot")
+public class SnapshotController {
+
+    @Autowired
+    private CoreSnapshotService snapshotServicel;
+
+    @RequestMapping("/save")
+    @ResponseBody
+    public String saveSnapshot() {
+        try {
+            this.snapshotServicel.makeSnapshot();
+        } catch (Throwable throwable){
+            throwable.printStackTrace();
+            return "error";
+        }
+        return "success";
+    }
+
+}
